f050f7c2c3a6629724f86d0059dc84a8f0382c88
Joey730 <87556566+Joey730@users.noreply.github.com>
2021-07-26 19:53:34 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -76,7 +76,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Movie Group', url='https://t.me/WorldMovieszZ')
+            InlineKeyboardButton('Movie Group', url='https://t.me/cinima_lokam_group')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
