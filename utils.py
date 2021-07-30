f2e64b30a70e70bf019b89105630aef53c02d37e
Joey730 <87556566+Joey730@users.noreply.github.com>
2021-07-30 12:04:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ async def save_file(media):
             file_size=media.file_size,
             file_type=media.file_type,
             mime_type=media.mime_type,
-            caption="@Moviesz_House",
+            caption="🔰Join🔰:@Moviesz_House",
         )
     except ValidationError:
         logger.exception('Error occurred while saving file in database')
