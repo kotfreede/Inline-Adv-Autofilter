6d54d867a9cc3e738d0e78f5f7570706229efa71
KOT BOTS <88032123+kotfreede@users.noreply.github.com>
2021-10-13 11:54:46 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -76,7 +76,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Update Channel', url='https://t.me/joinchat/Wl2qJsJpNhg0OWVl')
+            InlineKeyboardButton('Update Channel', url='https://t.me/KOT_BOTS')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
